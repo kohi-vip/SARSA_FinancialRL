5dc8f3252ee2c6313cd434599e31638556653850
kohi-vip <lamquangkhoi2016@gmail.com>
2025-08-03 16:43:46 +0700

    version 0.1

---
+++ main.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+import sys
+sys.stdout.reconfigure(encoding='utf-8')
+
+def hello_sarsa():
+    print("Chào SARSA Agent!")
+    print("Chúng ta đang cộng tác trên GitHub 🎯")
+
+if __name__ == "__main__":
+    hello_sarsa()
